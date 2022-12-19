3c5224b60f80de84902b3fecddd0f03bd3e4178e
Valeriia.Kurylko <reviliya@gmail.com>
2022-12-19 11:35:18 +0100

    hashed password working

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require("express");
+const { hashPassword } = require("./auth.js");
+const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
 const app = express();
 
 const port = 5000;
+
+const routes = require('express').Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUsersById);
+
+app.post("/api/users", jsonParser, hashPassword, userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
@@ -22,3 +32,6 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+
+
